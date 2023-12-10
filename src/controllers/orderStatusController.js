a50cf0d306129ae6879354708af1337ce9e74135
furqanx11 <ahmedfurqan510@gmail.com>
2023-12-10 20:52:33 +0500

    Add orderStatusController and order_status route

---
+++ src/controllers/orderStatusController.js
@@ -0,0 +1,57 @@
+const db = require('../../db');
+
+  
+function calculatePercentageRemaining(lat1, lon1, lat2, lon2) {
+    return ((lat1 - lat2) / (lon1 - lon2))*100;
+  }
+  
+  
+module.exports.order_status = (req, res) => {
+   
+    db.query('SELECT * FROM order_test WHERE o_id = ?', [req.params.id], (err, orders) => {
+        if (err) throw err;
+
+        if (orders.length === 0) {
+            return res.status(404).send('Order not found');
+        }
+
+        const order = orders[0];
+
+       
+        db.query('SELECT latitude, longitude FROM customer WHERE CustId = ?', [order.CustomerId], (err, results) => {
+            if (err) throw err;
+
+            const customerLocation = results[0];
+
+            if (!customerLocation) {
+                return res.status(404).send('Location not found');
+            }
+
+            console.log(customerLocation);
+            
+            
+
+            const storeLocation = { latitude: 24.8067, longitude: 67.0284 }; 
+
+            const percentageRemaining = calculatePercentageRemaining(customerLocation.latitude, customerLocation.longitude, storeLocation.latitude, storeLocation.longitude);
+            currentDistance = (`${percentageRemaining.toFixed(2)}% distance remaining.`);
+            console.log(currentDistance);
+
+            
+            let status;
+            if (currentDistance >= 100) {
+                status = 'Delivered';
+            } else if (currentDistance >= 80) {
+                status = 'Almost there';
+            } else if (currentDistance >= 50) {
+                status = 'In transit';
+            } else if (currentDistance >= 10) {
+                status = 'Dispatched';
+            } else {
+                status = 'Processing';
+            }
+
+            res.send({ status });
+        });
+    });
+};
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const {login} = require('../controllers/loginController');
 const {signup} = require('../controllers/signUpController');
+const {order_status} = require('../controllers/orderStatusController');
 
 router.post('/login', login);
 router.post('/signup', signup);
+router.get('/order_status/:id', order_status);
 
 module.exports = router;
