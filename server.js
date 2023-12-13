2a800cb4c8f69cdc5dc7321d485a4074f0345e97
furqanx11 <ahmedfurqan510@gmail.com>
2023-12-13 19:22:33 +0500

    Add chat functionality

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 require('dotenv/config');
 const express = require('express');
 const authRoutes = require('./src/routes/authRoutes');
-const productRoutes = require('./src/routes/productRoutes');
 const db = require('./db');
 
 const app = express();
 app.use(express.json());
 
 app.use('/auth', authRoutes);
-app.use('/product', productRoutes);
 
 app.listen(3000, () => console.log('Server is running...'));
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const path = require("path");
+
 const {login} = require('../controllers/loginController');
 const {signup} = require('../controllers/signUpController');
 const {order_status} = require('../controllers/orderStatusController');
+const {create, listing, getSingle} = require('../controllers/productController');
 
+router.post('/create', create);
+router.get('/all', listing);
+router.get('/:id', getSingle);
 router.post('/login', login);
 router.post('/signup', signup);
 router.get('/order_status/:id', order_status);
